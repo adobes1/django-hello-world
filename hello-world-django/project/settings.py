38eb74f2e1bb7a84285f73453e695030bff40923
Adam Dobes <adobes@redhat.com>
2024-01-10 20:22:45 +0100

    Allow all hosts and disable debug

--- hello-world-django/project/settings.py
+++ hello-world-django/project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rnt++cm4bw#81)97ot)h!a2*hajc*!q(d344ct&5m7ew8w$eib'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
